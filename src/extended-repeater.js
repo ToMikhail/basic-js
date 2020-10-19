5694cc3ca7efc13e0b1fe81505ef4d40031cb00e
To Mikha <toustsikmikhail@gmail.com>
2020-10-19 11:15:14 +0300

    feat: add function extender-repeater (ALL)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,8 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  let newStr = '';
+  if(options.addition === false) {
+    options.addition = 'false';
+  }
+  else if (options.addition === null) {
+    options.addition = 'null';
+  }
+  else {
+    options.separator = options.separator || '+';
+    options.additionSeparator = options.additionSeparator || '|';
+    options.repeatTimes = options.repeatTimes || 1;
+    options.additionRepeatTimes = options.additionRepeatTimes || 1;
+    options.addition = options.addition || '';
+  }
   
+  
+  let newStr = '';
+
   for (let i = 1; i <= options.repeatTimes; i++) {
     newStr = newStr + str;
 
