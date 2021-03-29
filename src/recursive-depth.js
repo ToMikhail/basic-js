3abde8c112ee3869af7942101b8e333e938ddb8f
Toustsik Mikahil <toustsikmikhail@gmail.com>
2021-03-29 11:14:34 +0300

    feat: add solutuions

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,18 +3,18 @@ const CustomError = require("../extensions/custom-error");
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
     let count = 0;
-    let res = 1;
+    let res = 0;
 
+    if (Array.isArray(arr)) {
     for (let i = 0; i < arr.length; i++) {
-      
-      if (Array.isArray(arr)) {
         count = this.calculateDepth(arr[i]);
         if (count > res) {
           res = count;
         }
       }
+      res++;
     }
-    res++;
+    
     return res;
   }
 };
