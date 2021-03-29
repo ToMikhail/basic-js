ed0e6b5df76009196855f714942a980f2ddf9531
Toustsik Mikahil <toustsikmikhail@gmail.com>
2021-03-29 10:04:29 +0300

    fix: add solution for dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,18 +4,27 @@ module.exports = function createDreamTeam(members) {
   let arr = [];
   let str = new String;
 
-  for (let i = 0; i < members.length; i++) {
+  if(Array.isArray(members)) {
+    for (let i = 0; i < members.length; i++) {
+    
+
     if (typeof members[i] === 'string') {
       members[i] = members[i].trim();
+      members[i] = members[i].toUpperCase();
       arr.push(members[i]);
-    }  
+    }
   }
 
-  let arrSort = arr.sort();
+    let arrSort = arr.sort();
 
-  for (let i = 0; i < arrSort.length; i++) {
-    str += arrSort[i].charAt(0);
-  } 
+    for (let i = 0; i < arrSort.length; i++) {
+      str += arrSort[i].charAt(0);
+    }
 
-  return str.toUpperCase();
-};
+    return str.toUpperCase();
+  }
+  
+  else {
+    return false;
+  }
+}
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,14 +2,19 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    let res = 0;
-    if(Array.isArray(arr)){
-  
-      for (let i = 0; i < arr.length; i++) {
-        res = Math.max(res, this.calculateDepth(arr[i]));
+    let count = 0;
+    let res = 1;
+
+    for (let i = 0; i < arr.length; i++) {
+      
+      if (Array.isArray(arr)) {
+        count = this.calculateDepth(arr[i]);
+        if (count > res) {
+          res = count;
+        }
       }
-      res++;
     }
-    return res; 
+    res++;
+    return res;
   }
 };
